540d255061b328fb1a563cd19eb8f2b5db748f77
coldfrontlich <coldfrontlich@gmail.com>
2023-02-12 18:17:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let text = expr.substring(i, i + 10);
+        if (text === '**********') {
+            result += ' ';
+            continue;
+        }
+        let element = '';
+        for (let j = 0; j < 10; j += 2) {
+            if (text.substring(j, j + 2) == '11') {
+                element += '-';
+            }
+            if (text.substring(j, j + 2) == '10') {
+                element += '.';
+            }
+        }
+        result += MORSE_TABLE[element];
+    }
+    return result;
 }
 
 module.exports = {
